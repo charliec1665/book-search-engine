de9a9d94b4aec20251e2f039b26e6d6efbc9499a
Charlie Carter <charliec1665@gmail.com>
2022-12-07 16:37:23 -0500

    added auth middleware and implemented Apollo Server

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,9 @@
+// import the gql tagged template function (advanced use of template literals)
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
