e71b4a213f942d9dfb527601aca8604c158a1b14
Charlie Carter <charliec1665@gmail.com>
2022-12-10 18:21:15 -0500

    added mutation hooks to book save and searches

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
+
 import Navbar from './components/Navbar';
 
 const httpLink = createHttpLink({
@@ -10,22 +13,34 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
+      <Router>
+        <>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<SearchBooks/>} />
+            <Route path='/saved' element={<SavedBooks/>} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Routes>
+        </>
+      </Router>
     </ApolloProvider>
   );
 }
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -73,6 +73,8 @@ const resolvers = {
 
                 return updatedUser;
             }
+
+            throw new AuthenticationError('You need to be logged in!')
         }
     }
 }
